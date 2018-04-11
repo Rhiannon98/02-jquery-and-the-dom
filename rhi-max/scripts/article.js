df0b5f3917af275b5003d8ec6f2f6102457c73ba
Rhiannon98 <mortensenrhi@gmail.com>
2018-04-11 10:08:47 -0700

    got rawDataObj to put stuff into an array

--- rhi-max/scripts/article.js
+++ rhi-max/scripts/article.js
@@ -2,15 +2,22 @@
 
 let articles = [];
 
+
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
 // PUT YOUR RESPONSE HERE
 
-function Article (rawDataObj) {
-  // TODO: Use the JS object that is passed in to complete this constructor function:
+function Article(rawDataObj) {
+  // Use the JS object that is passed in to complete constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorURL = rawDataObj.authorURL;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
 }
 
-Article.prototype.toHtml = function() {
+Article.prototype.toHtml = function () {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
   // PUT YOUR RESPONSE HERE
 
@@ -20,7 +27,7 @@ Article.prototype.toHtml = function() {
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance. (article [i])
     We need to fill in:
       1. author name,
       2. author url,
@@ -29,22 +36,22 @@ Article.prototype.toHtml = function() {
       5. publication date. */
 
   // REVIEW: Display the date as a relative number of 'days ago'
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
   $newArticle.append('<hr>');
   return $newArticle;
 };
 
-rawData.sort(function(a,b) {
+rawData.sort(function (a, b) {
   // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
+for (let i = 0; i < rawData.length; i++) {
   articles.push(new Article(rawData[i]));
 }
 
-for(let i = 0; i < articles.length; i++) {
+for (let i = 0; i < articles.length; i++) {
   $('#articles').append(articles[i].toHtml());
 }
