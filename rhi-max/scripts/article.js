f9a7df868359d3e5846d13a240edc72e6020774e
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-04-11 11:58:44 -0700

    still working on the filling in the details of the new articles and then appending them to the DOM

--- rhi-max/scripts/article.js
+++ rhi-max/scripts/article.js
@@ -12,7 +12,7 @@ function Article(rawDataObj) {
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
   this.author = rawDataObj.author;
-  this.authorURL = rawDataObj.authorURL;
+  this.authorURL = rawDataObj.authorUrl;
   this.publishedOn = rawDataObj.publishedOn;
   this.body = rawDataObj.body;
 }
@@ -22,13 +22,21 @@ Article.prototype.toHtml = function () {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
   // PUT YOUR RESPONSE HERE
 
-  let $newArticle = $('article.template').clone().appendTo('#articles'); // added .appendTo()
+  let $newArticle = $('article.template').clone().removeClass('.template'); // makes a new article section html PER the function call
 
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
 
-  if (!this.publishedOn) $newArticle.addClass('draft');
+  if (!this.publishedOn) $newArticle.addClass('draft'); //if this doesnt have a publishedOn THEN add class "draft"
+  //style the draft class display none
+
   $newArticle.attr('data-category', this.category);
+  //find() method
+  $newArticle.find('address > a').append(this.author, this.authorUrl);
+  $newArticle.find('h1').append(this.title);
+  $newArticle.find('.article-body').append(this.body);
+  $newArticle.attr('date-time').append(this.publishedOn);
+
 
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance. (article [i])
     We need to fill in:
@@ -44,6 +52,8 @@ Article.prototype.toHtml = function () {
 
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
   $newArticle.append('<hr>');
+
+  $('.articles').append()
   return $newArticle;
 };
 
@@ -54,9 +64,9 @@ rawData.sort(function (a, b) {
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for (let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
+// for (let i = 0; i < rawData.length; i++) {
+//   articles.push(new Article(rawData[i]));
+// }
 
 // rawData.forEach(function (val, i) {
 //   articles.push(new Article(rawData[i]))
@@ -72,10 +82,12 @@ for (let i = 0; i < rawData.length; i++) {
 //   console.log(i, val);
 // });
 
-for (let i = 0; i < articles.length; i++) {
-  $('#articles').append(articles[i].toHtml());
-}
+// for (let i = 0; i < articles.length; i++) {
+//   $('#articles').append(articles[i].toHtml());
+// }
 
 // articles.forEach(function (val, i) {
 //   $('#articles').append(articles[i].toHtml());
 // });
+
+articles.push(new Article(rawData[0]));
